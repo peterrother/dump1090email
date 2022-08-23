50a683c4e372b8c5f98696cc941c1ca1c839e127
Peter Rother <me@peterrother.com>
2022-08-23 13:20:55 -0400

    Add flight number to email subject

--- dump1090email.sh
+++ dump1090email.sh
@@ -57,7 +57,7 @@ do
 		if grep -Fxq "$DUP_REDUCT" $PROCESSED_FILE; then
 			continue
 		fi
-		
+
 		if [[ "$FLIGHT" == "null" ]]; then
 			continue
 		fi
@@ -66,7 +66,7 @@ do
 		echo $LAST_AIRCRAFT >> $PROCESSED_FILE
 
 		MSG="Timestamp: $DATE \nFlight: $FLIGHT \nHex Code: $HEX \nAltitude: $ALTITUDE ft\nVertical Speed: $VERT_RATE fpm\nTrack: $TRACK°\nGround Speed: $SPEED kts\n\nfr24://$FLIGHT\nhttp://flightradar24.com/$FLIGHT"
-		`/usr/bin/mail -s "Aircraft Spotted" -aFrom:"$MY_RADAR 1090MHz Feed <$FROM_EMAIL>" $TO_EMAIL <<< $(echo -e "$MSG")`
+		`/usr/bin/mail -s "Aircraft Spotted ($FLIGHT)" -aFrom:"$MY_RADAR 1090MHz Feed <$FROM_EMAIL>" $TO_EMAIL <<< $(echo -e "$MSG")`
 	done
 done
 
